dc0905cd586a1a68445d0188278fb290bb0d1d36
Bert Frees <bertfrees@gmail.com>
2016-09-21 13:36:28 +0200

    Add goal "generate-spi-classes"
    For each class annotated as an OSGi component, generates a wrapper class
    with an entry in META-INF/services, which performs binding of references
    itself using ServiceLoader.load()

---
+++ ds-to-spi-annotations-processor/src/main/java/org/daisy/pipeline/build/annotations/ComponentModel.java
@@ -0,0 +1,95 @@
+package org.daisy.pipeline.build.annotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComponentModel {
+	
+	String packageName;
+	String className;
+	String qualifiedClassName;
+	String spiClassName;
+	ActivateModel activate;
+	final List<PropertyModel> properties = new ArrayList<PropertyModel>();
+	final List<String> services = new ArrayList<String>();
+	final List<ReferenceModel> references = new ArrayList<ReferenceModel>();
+	
+	public String getPackageName() {
+		return packageName;
+	}
+	
+	public String getClassName() {
+		return className;
+	}
+	
+	public String getQualifiedClassName() {
+		return qualifiedClassName;
+	}
+	
+	public String getSpiClassName() {
+		return spiClassName;
+	}
+	
+	public List<PropertyModel> getProperties() {
+		return properties;
+	}
+	
+	public ActivateModel getActivate() {
+		return activate;
+	}
+	
+	public List<ReferenceModel> getReferences() {
+		return references;
+	}
+	
+	public static class ActivateModel {
+		
+		String methodName;
+		Class<?> propertiesArgumentType;
+		
+		public String getMethodName() {
+			return methodName;
+		}
+		
+		public Class<?> getPropertiesArgumentType() {
+			return propertiesArgumentType;
+		}
+	}
+	
+	public static class ReferenceModel {
+		
+		String methodName;
+		String service;
+		String cardinality;
+		
+		public String getMethodName() {
+			return methodName;
+		}
+		
+		public String getService() {
+			return service;
+		}
+		
+		public String getCardinality() {
+			return cardinality;
+		}
+	}
+	
+	public static class PropertyModel {
+		
+		Class<?> type;
+		String key;
+		Object value;
+		
+		public String getKeyLiteral() {
+			return "\"" + key.replaceAll("\"", "\\\"") + "\"";
+		}
+		
+		public String getValueLiteral() {
+			if (type == String.class)
+				return "\"" + value.toString().replaceAll("\"", "\\\"") + "\"";
+			else
+				return value.toString();
+		}
+	}
+}

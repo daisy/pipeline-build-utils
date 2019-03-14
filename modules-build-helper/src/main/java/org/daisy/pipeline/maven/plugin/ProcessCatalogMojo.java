Notalib/pipeline

Change how registering of modules works
Instead of the framework tracking bundles and processing modules, the
modules are now responsible for exposing themselves as Module
services. The logic for doing this is generated by the
modules-build-helper.

utils/build-utils/modules-build-helper/src/main/java/org/daisy/pipeline/maven/plugin/ProcessCatalogMojo.java
@@ -56,9 +56,21 @@ public class ProcessCatalogMojo extends AbstractMojo {
 	
 	@Parameter(
 		readonly = true,
+		defaultValue = "${project.artifactId}"
+	)
+	private String projectArtifactId;
+	
+	@Parameter(
+		readonly = true,
 		defaultValue = "${project.version}"
 	)
 	private String projectVersion;
+	
+	@Parameter(
+		readonly = true,
+		defaultValue = "${project.name}"
+	)
+	private String projectName;
 	
 	@Parameter(
 		readonly = true,
@@ -73,7 +85,9 @@ public class ProcessCatalogMojo extends AbstractMojo {
 			Map<String,String> options = new HashMap<String,String>(); {
 				options.put("generatedResourcesDirectory", asURI(generatedResourcesDirectory).toASCIIString());
 				options.put("generatedSourcesDirectory", asURI(generatedSourcesDirectory).toASCIIString());
+				options.put("moduleName", projectArtifactId);
 				options.put("moduleVersion", projectVersion);
+				options.put("moduleTitle", projectName);
 			}
 			engine.run(asURI(this.getClass().getResource("/process-catalog/process-catalog.xpl")).toASCIIString(),
 			           ImmutableMap.of("source", (List<String>)ImmutableList.of(asURI(catalogFile).toASCIIString())),

daisy/pipeline-build-utils

modules-build-helper/src/main/java/org/daisy/pipeline/maven/plugin/ProcessCatalogMojo.java
@@ -55,9 +55,19 @@ public class ProcessCatalogMojo extends AbstractMojo {
 	private boolean addSources;
 	
 	@Parameter(
+		defaultValue = "${project.artifactId}"
+	)
+	private String moduleName;
+	
+	@Parameter(
 		defaultValue = "${project.version}"
 	)
 	private String moduleVersion;
+	
+	@Parameter(
+		defaultValue = "${project.name}"
+	)
+	private String moduleTitle;
 	
 	@Parameter(
 		readonly = true,
@@ -72,7 +82,9 @@ public class ProcessCatalogMojo extends AbstractMojo {
 			Map<String,String> options = new HashMap<String,String>(); {
 				options.put("generatedResourcesDirectory", asURI(generatedResourcesDirectory).toASCIIString());
 				options.put("generatedSourcesDirectory", asURI(generatedSourcesDirectory).toASCIIString());
+				options.put("moduleName", moduleName);
 				options.put("moduleVersion", moduleVersion);
+				options.put("moduleTitle", moduleTitle);
 			}
 			engine.run(asURI(this.getClass().getResource("/process-catalog/process-catalog.xpl")).toASCIIString(),
 			           ImmutableMap.of("source", (List<String>)ImmutableList.of(asURI(catalogFile).toASCIIString())),